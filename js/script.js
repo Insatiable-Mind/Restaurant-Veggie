d591e8d701079367605f666ab28d3b841450203a
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-07-17 18:58:58 +0300

    Restore AJAX

--- js/script.js
+++ js/script.js
@@ -9,52 +9,52 @@
 
 
   /* Loading menu */
-  // var xhttp = new XMLHttpRequest();
-  // xhttp.onreadystatechange = function() {
-  //   if (this.readyState == 4 && this.status == 200) {
-  //     var $menu = $(".menu__items"),
-  //       json = JSON.parse(this.responseText);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var $menu = $(".menu__items"),
+        json = JSON.parse(this.responseText);
 
-  //     showMenuItems("starters");
+      showMenuItems("starters");
 
-  //     openMenu("starters");
-  //     openMenu("main-dishes");
-  //     openMenu("desserts");
-  //     openMenu("drinks"); 
-  //   }
+      openMenu("starters");
+      openMenu("main-dishes");
+      openMenu("desserts");
+      openMenu("drinks"); 
+    }
 
-  //   function showMenuItems(menuPart) {
-  //     var $list = $("<ul></ul>"),
-  //       $listItem = null;
+    function showMenuItems(menuPart) {
+      var $list = $("<ul></ul>"),
+        $listItem = null;
 
-  //     for(var i = 0; i < Object.keys(json[menuPart]).length; i++) {
-  //       $listItem = $("<li class='col-sm-6'></li>");
-  //       $listItem.append("<span class='menu__item'>" + json[menuPart][i].name + "</span>")
-  //         .append("<span class='menu__item-price'>" + json[menuPart][i].price + "</span>")
-  //         .append("<span class='menu__item-ingredients'>" + json[menuPart][i].ingredients + "</span>")
-  //         .appendTo($list);
-  //       $list.appendTo($menu);
-  //     }
-  //   }
+      for(var i = 0; i < Object.keys(json[menuPart]).length; i++) {
+        $listItem = $("<li class='col-sm-6'></li>");
+        $listItem.append("<span class='menu__item'>" + json[menuPart][i].name + "</span>")
+          .append("<span class='menu__item-price'>" + json[menuPart][i].price + "</span>")
+          .append("<span class='menu__item-ingredients'>" + json[menuPart][i].ingredients + "</span>")
+          .appendTo($list);
+        $list.appendTo($menu);
+      }
+    }
 
-  //   function openMenu(menuPart) {
-  //     $(".menu__" + menuPart).on("click", function(e){
-  //       e.preventDefault();
-  //       $menu.html("");
+    function openMenu(menuPart) {
+      $(".menu__" + menuPart).on("click", function(e){
+        e.preventDefault();
+        $menu.html("");
 
-  //       if (menuPart === "main-dishes") {
-  //         menuPart = "mainDishes";
-  //       }
+        if (menuPart === "main-dishes") {
+          menuPart = "mainDishes";
+        }
 
-  //       showMenuItems(menuPart);
-  //       $(".menu__nav a").removeClass("active");
-  //       $(".menu__" + menuPart).addClass("active");
-  //     });
-  //   }
-  // };
+        showMenuItems(menuPart);
+        $(".menu__nav a").removeClass("active");
+        $(".menu__" + menuPart).addClass("active");
+      });
+    }
+  };
 
-  //   xhttp.open("GET", "../menu.json", true);
-  //   xhttp.send();
+    xhttp.open("GET", "../menu.json", true);
+    xhttp.send();
 
     /* Preventing default behavior for submit button in form */
     $(".contact__form").on("submit", function(e){
