f1bc311e4a62816bde5996e5afc62568c271eb28
Insatiable_Mind <o.a.chernyakova@gmail.com>
2017-07-17 18:51:02 +0300

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,30 @@ var browserSync = require("browser-sync").create();
 /* Static Server + watching scss/html files */
 gulp.task("serve", ["sass"], function() {
 
-    // browserSync.init({
-    //     server: "./"
-    // });
+  browserSync.init({
+    server: "./"
+  });
 
-    bs.init({
-        server: "./"
-    });
-
-    //gulp.watch("*.html").on("change", browserSync.reload);
-    //gulp.watch("sass/*.scss", ["sass"]).on("change", browserSync.reload);
-    gulp.watch("*.html").on("change", bs.reload);
-    gulp.watch("sass/*.scss", ["sass"]).on("change", bs.reload); //<---changed it with the line below
-    //gulp.watch("sass/*.scss", ["sass"]);
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch("sass/*.scss", ["sass"]).on("change", browserSync.reload);
 });
 
 /* Compile Sass into CSS & auto-inject into browsers */
 gulp.task("sass", function() {
-    return gulp.src("sass/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream()); //<---changed it with the line belowgulpo
-        //.pipe(browserSync.reload({stream: true}));
+  return gulp.src("sass/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
 });
 
 /* Concatenate and minify CSS */
 gulp.task("css:build", function() {
   gulp.watch("css/*.css").on("change", function() {
     return gulp.src("css/*.css")
-    .pipe(concat("style.concat.css"))
-    .pipe(csso({
-      comments: false
-    }))
+      .pipe(concat("style.concat.css"))
+      .pipe(csso({
+        comments: false
+      }))
     .pipe(gulp.dest("css_result"));
   });
 });
